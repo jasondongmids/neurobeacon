a2d15595c9987bfa433a500779372264ef25c4e6
JDong <djliang@berkeley.edu>
2025-03-20 14:01:26 -0700

    integrate login statistics behavior

--- src/components/Redirection.jsx
+++ src/components/Redirection.jsx
@@ -1,11 +1,14 @@
 import { useNavigate, useLocation } from "react-router-dom";
 import { useContext, useEffect } from "react";
 import UserContext from "../context/UserContext";
+import UserStatisticsContext from "../context/UserStatisticsContext"
 
 const Redirection = ({ children }) => {
     const navigate = useNavigate();
     const location = useLocation();
     const { checkAuth } = useContext(UserContext);
+    const { userStats, setUserStats, dailyStats, setDailyStats, weeklyStats, setWeeklyStats,
+        queryStats, addStats, parseNestedJson, updateStreak, formatDate } = useContext(UserStatisticsContext)
     
     useEffect(() => {
         const handleRedirection = async () => {
@@ -13,6 +16,40 @@ const Redirection = ({ children }) => {
             console.log("Authenticated", isAuth)
             if (!isAuth) {
                 navigate("/", { state: { from: location.pathname } });
+            }
+
+            if (isAuth) {
+                const stats = await queryStats("", 1)
+                const daily = await queryStats("daily", 1)
+                const weekly = await queryStats("weekly", 1)
+    
+                if (stats) {
+                  setUserStats(parseNestedJson(stats[0]))
+                } else {
+                  addStats("", JSON.stringify(userStats))
+                }
+    
+                if (daily) {
+                  if (formatDate(daily[0].updated_at) < formatDate(Date.now())) {
+                    const newStats = updateStreak(daily[0])
+                    setDailyStats(parseNestedJson(newStats))
+                  } else {
+                    setDailyStats(parseNestedJson(daily[0]))
+                  }
+                } else {
+                  addStats("daily", JSON.stringify(dailyStats))
+                } 
+    
+                if (weekly) {
+                  if (formatDate(daily[0].updated_at) < formatDate(Date.now())) {
+                    const newStats = updateStreak(weekly[0])
+                    setWeeklyStats(parseNestedJson(newStats))
+                  } else {
+                    setWeeklyStats(parseNestedJson(weekly[0]))
+                  }
+                } else {
+                  addStats("weekly", JSON.stringify(weeklyStats))
+                } 
             }
 
             if (isAuth && location.pathname === "/") {
--- src/context/UserStatisticsContext.jsx
+++ src/context/UserStatisticsContext.jsx
@@ -1,16 +1,24 @@
-import React, { createContext, useState } from "react";
+import React, { createContext, useEffect, useState } from "react";
 import { dataClient } from "..";
+
+// User statistics are loaded/updated during these actions:
+// 1) User login - loads user/daily/weekly stats; if no stats, writes stats
+// 2) After each game - updates user/daily/weekly stats
+// 3) User logout - updates user/daily/weekly stats (not necessary due to option 2)
+// Beyond MVP: enhancement only write after each session, tab close, or refresh
 
 const UserStatisticsContext = createContext();
 
 export const UserStatisticsProvider = ({children}) => {
     // ✅ Create react states
-    const [dailyStats, setDailyStats] = useState({
+
+    const schema = {
+        yyyymmdd: '',
         current_streak: 0,
         longest_streak: 0,
         // days_on_platform: a.integer(),
         // sk: "",
-        total_sessions: 0,
+        // total_sessions: 0,
         total: {
             total_questions: 0,
             total_correct: 0,
@@ -46,52 +54,30 @@ export const UserStatisticsProvider = ({children}) => {
             total_correct: 0,
             percent_correct: 0.0,
         },
-    });
+    }
 
-    const [weeklyStats, setWeeklyStats] = useState({
-        current_streak: 0,
-        longest_streak: 0,
-        // days_on_platform: a.integer(),
-        // sk: "",
-        total_sessions: 0,
-        total: {
-            total_questions: 0,
-            total_correct: 0,
-            percent_correct: 0.0,            
-        },
-        math: {
-            total_questions: 0,
-            total_correct: 0,
-            percent_correct: 0.0,
-        },
-        visual: {
-            total_questions: 0,
-            total_correct: 0,
-            percent_correct: 0.0, 
-        },
-        reaction: {
-            total_questions: 0,
-            total_correct: 0,
-            percent_correct: 0.0,         
-        },
-        easy: {
-            total_questions: 0,
-            total_correct: 0,
-            percent_correct: 0.0,
-        },
-        medium: {
-            total_questions: 0,
-            total_correct: 0,
-            percent_correct: 0.0,
-        },
-        hard: {
-            total_questions: 0,
-            total_correct: 0,
-            percent_correct: 0.0,
-        },
-    });
-
+    const [dailyStats, setDailyStats] = useState(structuredClone(schema))
+    const [weeklyStats, setWeeklyStats] = useState(structuredClone(schema))
+    const [userStats, setUserStats] = useState(structuredClone(schema))
     const [queryStatistics, setQueryStatistics] = useState('')
+
+    // Write to database before tab closes or refreshed 
+    // Beyond MVP; need to wrap graphQL query into JSON message instead of using function
+    // useEffect(() => {
+    //     const handleBeforeUnload = (event) => {
+    //         event.preventDefault();
+
+    //         navigator.sendBeacon(updateStats("daily", dailyStats))
+    //         navigator.sendBeacon(updateStats("weekly", weeklyStats))
+    //         navigator.sendBeacon(updateStats("", userStats))
+    //     };
+
+    //     window.addEventListener('beforeunload', handleBeforeUnload);
+
+    //     return () => {
+    //         window.removeEventListener('beforeunload', handleBeforeUnload);
+    //     };
+    // }, []);
 
     // ✅ Date functions
     const formatDate = (unixTimestamp) => {
@@ -113,16 +99,31 @@ export const UserStatisticsProvider = ({children}) => {
     const getYearMonthDate = (frequency) => {
         const yyyymmdd = frequency === 'daily' ? formatDate(Date.now())
                 : frequency === 'weekly' ? getStartOfWeek()
-                : Date.now()
+                : formatDate(Date.now())
 
         return yyyymmdd
     }
 
+    // parse JSON response
+    const parseNestedJson = (data) => {
+        return Object.entries(data).reduce((acc, [key, value]) => {
+          try {
+            acc[key] = JSON.parse(value);
+          } catch {
+            acc[key] = value;
+          }
+          return acc;
+        }, {});
+      };
+
     // ✅ Update React state
-    const updateDailyStatsState = (newDailyStatistics, gameType, difficulty) => {
-        const { correct } = newDailyStatistics;
+    const updateStatsState = (frequency, gameType, difficulty, newStatistics ) => {
+        const { correct } = newStatistics;
+        const setStatsSetter = frequency === 'daily' ? setDailyStats
+            : frequency === 'weekly' ? setWeeklyStats
+            : setUserStats;
 
-        setDailyStats(prevStats => {
+        setStatsSetter(prevStats => {
             const totalQuestions = prevStats.total.total_questions + 1;
             const totalCorrect = correct ? prevStats.total.State.total_correct + 1 : prevStats.total.total_correct;
 
@@ -159,45 +160,16 @@ export const UserStatisticsProvider = ({children}) => {
         })
     };
 
-    const updateWeeklyStatsState = (newWeeklyStatistics, daily, difficulty) => {
-        const { correct } = newWeeklyStatistics;
-
-        setWeeklyStats(prevStats => {
-            const totalQuestions = prevStats.total.total_questions + 1;
-            const totalCorrect = correct ? prevStats.total.State.total_correct + 1 : prevStats.total.total_correct;
-
-            const gameQuestions = prevStats[gameType].total_questions + 1;
-            const gameCorrect = correct ? prevStats[gameType].total_correct + 1 : prevStats[gameType].total_correct;
-
-            const diffQuestions = prevStats[difficulty].total_questions + 1;
-            const diffCorrect = correct ? prevStats[difficulty].total_correct + 1 : prevStats[difficulty].total_correct;
-
-            return {
-                ...prevStats,
-                total: {
-                    total_questions: totalQuestions,
-                    total_correct: totalCorrect,
-                    percent_correct: totalQuestions > 0
-                        ? parseFloat((totalCorrect / totalQuestions).toFixed(3))
-                        : 0.0,
-                },
-                [gameType]: {
-                    total_questions: gameQuestions,
-                    total_correct: gameCorrect,
-                    percent_correct: gameQuestions > 0
-                        ? parseFloat((gameCorrect / gameQuestions).toFixed(3))
-                        : 0.0,
-                },
-                [difficulty]: {
-                    total_questions: diffQuestions,
-                    total_correct: diffCorrect,
-                    percent_correct: diffQuestions > 0
-                    ? parseFloat((diffCorrect / diffQuestions).toFixed(3))
-                    : 0.0,
-                },
-            }
-        })
-    };
+    // Update current streak and longest streak on initial login
+    const updateStreak = (inputData) => {
+        const newStreak = inputData.current_streak + 1
+        const longestStreak = inputData.longest_streak
+        return {
+            ...inputData,
+            current_streak: newStreak,
+            longest_streak: newStreak > longestStreak ? newStreak : longestStreak
+        }
+    }
 
     // ✅ Add statistics
     const addStats = async (frequency, inputData) => {
@@ -213,7 +185,7 @@ export const UserStatisticsProvider = ({children}) => {
             if (errors) {
                 console.error('Check inputs or CloudWatch logs:', errors);
             } else {
-                console.log('Successful add', data);
+                console.log(`Successful ${frequency} add`, data);
             }
         } catch (error) {
             console.error('Error with function in UserStatisticsContext.jsx', error)
@@ -252,8 +224,9 @@ export const UserStatisticsProvider = ({children}) => {
             if (errors) {
                 console.error('Check inputs or CloudWatch logs:', errors);
             } else {
-                console.log('Successful query', data);
+                console.log(`Successful ${frequency} query`, data);
                 setQueryStatistics(data)
+                return data
             }
         } catch (error) {
             console.error('Error with function in UserStateContext.jsx:', error)
@@ -262,18 +235,18 @@ export const UserStatisticsProvider = ({children}) => {
 
     const updateStats = async(frequency, inputData) => {
         try {
-            const yyyymmdd = getYearMonthDate(frequency)
+            // const yyyymmdd = getYearMonthDate(frequency)
 
             const { data, errors } = await dataClient.mutations.updateStats({
                 frequency: frequency,
-                yyyymmdd: yyyymmdd,
+                // yyyymmdd: yyyymmdd,
                 data: inputData
             });
 
             if (errors) {
                 console.error('Check inputs or CloudWatch logs:', errors);
             } else {
-                console.log('Successful update', data);
+                console.log(`Successful ${frequency} update`, data);
             }
         } catch (error) {
             console.error('Error with function in UserStatisticsContext.jsx', error)
@@ -283,15 +256,22 @@ export const UserStatisticsProvider = ({children}) => {
     return (
         <UserStatisticsContext.Provider value ={{
             dailyStats,
+            setDailyStats,
             weeklyStats,
+            setWeeklyStats,
+            userStats,
+            setUserStats,
             queryStatistics,
             setQueryStatistics,
-            updateDailyStatsState,
-            updateWeeklyStatsState,
+            updateStatsState,
+            // updateWeeklyStatsState,
             addStats,
             getStats,
             queryStats,
             updateStats,
+            parseNestedJson,
+            updateStreak,
+            formatDate,
         }}>
             { children }
         </UserStatisticsContext.Provider>
