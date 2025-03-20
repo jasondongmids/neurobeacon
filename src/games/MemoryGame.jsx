3f7176b0e3a2c5874c06f0bd1bfd7ce58b75e496
fehayes <142453386+fehayes@users.noreply.github.com>
2025-03-19 20:36:24 -0500

    Update MemoryGame.jsx

--- src/games/MemoryGame.jsx
+++ src/games/MemoryGame.jsx
@@ -303,7 +303,7 @@ const MemoryGame = forwardRef(({ onUpdateStats }, ref) => {
         setTimeout(() => {
           setShowFridge(false);
           setGamePhase("showKitchen");
-        }, 3000);
+        }, 100000);
       }, 1000);
     } else if (gamePhase === "showKitchen") {
       setTimeout(() => {
@@ -314,7 +314,7 @@ const MemoryGame = forwardRef(({ onUpdateStats }, ref) => {
           setStartTime(Date.now());
           // Reset attemptTimes for this question.
           attemptTimesRef.current = [];
-        }, 3000);
+        }, 1000000);
       }, 1000);
     }
   }, [gamePhase]);
