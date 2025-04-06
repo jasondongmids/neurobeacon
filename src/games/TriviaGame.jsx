7b9674eb55d2bdbcffb5ec33e5873fe7575554ba
fehayes <142453386+fehayes@users.noreply.github.com>
2025-04-05 19:09:15 -0500

    Update TriviaGame.jsx

--- src/games/TriviaGame.jsx
+++ src/games/TriviaGame.jsx
@@ -476,7 +476,7 @@ const TriviaGame = forwardRef(({ onUpdateStats }, ref) => {
       <div style={{ color: "white", margin: "16px 0", fontSize: "1.2em" }}>
         <h2 style={{ fontSize: "1.4em" }}>Game Rules:</h2>
         <p>Answer trivia questions from your selected decades by clicking on the answer followed by the Submit Answer Button.</p>
-        <p>Points are awarded based on difficulty and speed. Test 12</p>
+        <p>Points are awarded based on difficulty and speed. Test 13</p>
         <p>Try to answer quickly to maximize your score!</p>
         <p>Feel free to click the Skip Question button to get a new question with no scoring penalty!</p>
       </div>
